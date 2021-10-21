a6718ec274005d6950245f364fd245412ca42846
nedumd <davidsonchinedum@gmail.com>
2021-10-21 17:09:49 +0100

    read eth balance

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "./components/layout";
+import ConnectButton from "./components/ConnectButton";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ChakraProvider>
+      <Layout>
+        <ConnectButton />
+      </Layout>
+    </ChakraProvider>
+  )
 }
-
-export default App;
